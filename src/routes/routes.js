730cf7c13964d0e1619515b05c08829933bcf7f2
nachorebolleda <rebolleda90@gmail.com>
2022-08-02 12:24:55 -0300

    asd

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,14 +4,14 @@ const DB = require('../db')
 
 // GET /alumnos
 router.get('/alumnos', async (req,res)=>{
-  res.send("Entré a la ruta /alumnos")
-    // const datos = await DB.Routes.getDatos();    
-    // res.status(200).json(datos)
+//  res.send("Entré a la ruta /alumnos")
+    const datos = await DB.query('SELECT * FROM alumnos')    
+    res.status(200).json(datos)
 });
 
 // GET /contenedores
 router.get('/contenedores', async (req,res)=>{
-    const datos = await DB.Routes.getInfo();    
+    const datos = await DB.query('SELECT * FROM contenedores')    
     res.status(200).json(datos)
 });
 
