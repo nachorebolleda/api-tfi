62893bbb5bdd1fd790ee4aba4af9e58bf503167b
nachorebolleda <rebolleda90@gmail.com>
2022-08-01 21:20:25 -0300

    asd

--- src/db/departments.db.js
+++ src/db/departments.db.js
@@ -14,20 +14,15 @@ const pool = require("./connection.db");
 // };
 
 
-module.exports.getDatos = async function () {
+module.exports.getDatos = function () {
   let conn;
   try {
-    conn = await pool.getConnection();
-    //const rows = await conn.query('SELECT * FROM alumnos');
-    return {
-      "squadName": "Super hero squad",
-      "homeTown": "Metro City",
-      "formed": 2016,
-      "secretBase": "Super tower"
-    }
+    conn = pool.getConnection();
+    const rows = conn.query('SELECT * FROM alumnos');
+    return rows;
   } catch (err) {
     return Promise.reject(err);
   } finally {
-    if (conn) await conn.release();
+    if (conn) conn.release();
   }
 };
