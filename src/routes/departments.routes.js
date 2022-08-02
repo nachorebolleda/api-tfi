c3da446d62bc9422e14d147a3e7d17a43437454e
nachorebolleda <rebolleda90@gmail.com>
2022-08-01 21:23:36 -0300

    asd

--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -5,7 +5,7 @@ const DB = require('../db')
 // GET /alumnos
 router.get('/alumnos', (req,res)=>{
     const datos = DB.Departmens.getDatos();    
-    res.json(datos)
+    res.status(200).json(datos)
 });
 
 module.exports=router
